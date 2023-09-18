b92b55a76e8c5ed3ea35203bcb6e6bc0834ae91e
Jonathan Whitmore <jbwhit@gmail.com>
2023-09-18 15:48:20 -0700

    Pass 2

--- tidytuesday002/_modidx.py
+++ tidytuesday002/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://jbwhit.github.io',
                 'git_url': 'https://github.com/jbwhit/tidytuesday002',
                 'lib_path': 'tidytuesday002'},
-  'syms': {'tidytuesday002.core': {'tidytuesday002.core.foo': ('core.html#foo', 'tidytuesday002/core.py')}}}
+  'syms': {'tidytuesday002.core': {'tidytuesday002.core.foo': ('first_pass.html#foo', 'tidytuesday002/core.py')}}}
